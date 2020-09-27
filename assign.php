98fdae04ee3a07717f147151c2077d4c4dea2842
Alex Brasil <alexfbrazil123@gmail.com>
2020-09-26 20:03:52 -0600

    Add files via upload

---
+++ assign.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body id="sec">
+<h1>Alex Brasil</h1>
+<?php
+    echo "Today's date " . date("Y/m/d") . "<br>";
+?>
+<hr>
+<table>
+<tr>
+    <td>
+        <a href="">Week02 (Coming Soon)</a><br>
+    </td>
+    <td>
+        <a href="">Week03 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week04 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week05 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week06 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week07 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week08 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week09 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week10 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week11 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week12 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week13 (Coming Soon)</a>
+    </td>
+    <td>
+        <a href="">Week14 (Coming Soon)</a>
+    </td>
+</tr>
+
+</table>
+</body>
+<hr>
+<footer>
+    <a class="button" href="home.php">Go to Home Page</a>
+</footer>
+</html>
---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+<h1>Alex Brasil</h1>
+<?php
+    echo "Today's date " . date("Y/m/d") . "<br>";
+?>
+<hr>
+<div>
+    <h3>Learning more about me!</h3>
+</div>
+<div>
+    <a href="https://www.facebook.com/alex.brazil.77" target="_blank">
+        <img src="IMG_4385.JPG" alt="Alex Brasil">
+    </a>
+</div>
+<div class="paragh">
+<pre>My name is Alex Brasil. I am 26 years old and single, but dating. 
+As my last name already says, I am from Brazil. My home town is called 
+Parnaiba, located in the northeast region of the country. I come from a 
+family of five people, my parents, my brother, my sister and I, which
+I am the youngest one. I came to US four years ago. At first, I came to 
+learn English, but now I've been in college for three years. My major is 
+Software Engineering. I want to work with software develpment and maybe 
+develop applications, I will see where my career will take me to. </pre>
+</div>
+<br>
+    <p><b>Watch my youtube Channel!</b></p>
+<a href="https://www.youtube.com/channel/UC-5N7PgDO_22jgm3RCq1_gw/featured?view_as=subscriber" target="_blank">
+    <img src="cha.jpg" alt="Programming God!">
+</a>
+</body>
+<hr>
+<footer>
+    <a class="button" href="assign.php">Go to Assignments Page</a>
+</footer>
+</html>
